1d5ca4a0975beca5a3f82b381c2464f123b95f5d
Shoyadbek <sh.abdurasulov@gmail.com>
2024-02-04 03:18:15 +0500

    first

---
+++ api/bot.js
@@ -0,0 +1,12 @@
+import { Bot, webhookCallback } from "grammy";
+
+const token = process.env.BOT_TOKEN;
+if (!token) throw new Error("BOT_TOKEN is unset");
+
+const bot = new Bot(token);
+
+bot.command('start', ctx => {
+    ctx.reply('Hello Vercel!')
+})
+
+export default webhookCallback(bot, "http");
