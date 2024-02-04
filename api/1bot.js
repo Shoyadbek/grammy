00e7f871954651e4161e9cf26e4ce960f7372142
Shoyadbek <sh.abdurasulov@gmail.com>
2024-02-04 07:01:59 +0500

    all

--- api/1bot.js
+++ api/1bot.js
@@ -9,4 +9,29 @@ bot.command('start', ctx => {
     ctx.reply('Hello Vercel!')
 })
 
+bot.command("private", (ctx) => {
+  ctx.reply("Using Web Application Button in private messages", {
+    ...Markup.inlineKeyboard([
+      Markup.button.webApp("Web App Button", process.env.PUBLIC_WEBAPP_URL),
+    ]),
+  });
+});
+
+bot.command("inline", (ctx) => {
+  ctx.reply("Using Inline Button in public messages", {
+    ...Markup.inlineKeyboard([
+      Markup.button.url("Inline Button", process.env.TELEGRAM_WEBAPP_URL),
+    ]),
+  });
+});
+
+bot.command("keyboard", (ctx) => {
+  ctx.reply("Using Keyboard Button in prviate messages", {
+    ...Markup.keyboard([
+      Markup.button.webApp("Keyboard Button", process.env.PUBLIC_WEBAPP_URL),
+    ]).resize(),
+  });
+});
+
+
 export default webhookCallback(bot, "http");
