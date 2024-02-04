a64fa843cf2db0984ccd5c24fa049c843a8e8157
Shoyadbek <sh.abdurasulov@gmail.com>
2024-02-04 19:51:57 +0500

    some changes

--- api/bot.js
+++ api/bot.js
@@ -2,6 +2,7 @@ import { webhookCallback } from "grammy";
 import bot from "../utils/bot.js";
 import registerHandlers from "../handlers/handlers.js";
 
+// Ваш код оставляется без изменений
 async function runApp() {
   // Handle errors
   bot.catch((err) => console.error(err));
@@ -13,7 +14,14 @@ async function runApp() {
   webhookCallback(bot, "http");
 
   // Start bot
-  console.info(`\x1b[33mBot is running on @${bot.botInfo.username}\x1b[0m`);
+  // console.info(`\x1b[33mBot is running on @${bot.botInfo.username}\x1b[0m`);
 }
 
-runApp();
+// Экспортируйте функцию, которая будет обрабатывать HTTP-запросы
+export default async function handler(req, res) {
+  // Вызовите вашу функцию runApp при получении HTTP-запроса
+  await runApp();
+
+  // Верните успешный статус, если нужно
+  res.status(200).send("Bot is running!");
+}
