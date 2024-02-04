538525b1fe7167e1f72cb0a7d9f9ef9fda0325a9
Shoyadbek <sh.abdurasulov@gmail.com>
2024-02-04 07:36:46 +0500

    restruct-locales

--- utils/i18n.js
+++ utils/i18n.js
@@ -1,6 +1,9 @@
 import { I18n } from "@grammyjs/i18n";
 import path from 'path';
 
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
+
 const i18n = new I18n({
   defaultLocale: "uz",
   directory: path.resolve(__dirname, "locales"),
