406ce9c841dfcecdd66b84d09386e2777ca39a47
Shoyadbek <sh.abdurasulov@gmail.com>
2024-02-06 05:19:15 +0500

    sm122

--- api/bot.js
+++ api/bot.js
@@ -1,10 +1,9 @@
 import { Telegraf } from "telegraf";
 
-
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.command("hello", (ctx) => ctx.reply("Hello, friend!"));
 
-module.exports = (req, res) => {
+export default (req, res) => {
   return bot.handleUpdate(req.body, res);
 };
